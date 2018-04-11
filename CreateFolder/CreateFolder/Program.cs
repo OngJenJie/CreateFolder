1e0eae426a27e89c930ab0787fa4f3dfc5be5bf2
CancerNeko <jenjieong@yahoo.com>
2018-04-11 23:41:11 +0800

    寫了簡單的創造文件夾的程序

---
+++ CreateFolder/CreateFolder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CreateFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string dirPath = @"C:\test\file";
+
+                if (Directory.Exists(dirPath))
+                {
+                    Console.WriteLine("The directory {0} already exists.", dirPath);
+                }
+                else
+                {
+                    Directory.CreateDirectory(dirPath);
+                    Console.WriteLine("The directory {0} was created.", dirPath);
+                }
+        }
+    }
+}
