b0cb24c6eba96fcac734374fd4c8deb9f696957e
CancerNeko <jenjieong@yahoo.com>
2018-04-24 20:02:40 +0800

    加入了註解

--- CreateFolder/CreateFolder/Program.cs
+++ CreateFolder/CreateFolder/Program.cs
@@ -11,12 +11,19 @@ namespace CreateFolder
     {
         static void Main(string[] args)
         {
+
+            // 讓使用者輸入文件夾數量
             Console.WriteLine("請輸入你想要創造多少文件夾：");
             int count = int.Parse(Console.ReadLine());
+
+            // 創建數個文件夾
             for (int i = 0; i < count; i++)
             {
+
+                // 設定地點
                 string dirPath = @"C:\test\file" + i;
 
+                // 如果那個位置已經存在文件夾就通知他
                 if (Directory.Exists(dirPath))
                 {
                     Console.WriteLine("▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅");
@@ -25,6 +32,8 @@ namespace CreateFolder
                     Console.WriteLine("▉                                         ▉  ");
                     Console.WriteLine("▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅");
                 }
+
+                // 如果沒有就創建文件夾
                 else
                 {
                     Directory.CreateDirectory(dirPath);
